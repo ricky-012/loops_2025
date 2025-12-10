d17435439fc90106603c7bf20971e91305d162c7
ricky-012 <ejherrera289@gmail.com>
2025-12-10 13:22:13 -0600

    Update 1_loops.py

--- 1_loops.py
+++ 1_loops.py
@@ -1,21 +1,79 @@
-# Example Practice:
-# Given this list of fruits:
-fruits = ["apple", "banana", "cherry", "date"]
+# # Example Practice:
+# # Given this list of fruits:
+# fruits = ["apple", "banana", "cherry", "date"]
 
-# Challenge:
-# Use a for loop to print each fruit on a new line.
+# print(len(fruits))
+# # Challenge:
+# # Use a for loop to print each fruit on a new line.
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+# for fruit in fruits:
+#     print(fruit)
+# # i just worked with loops
 
-
-# Given a list of school subjects:
+# # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
+for subject in subjects:
+    print(subject)
+
+# # Print each subject but stop when History is reached
+# # for subject in subjects:
+# #     if subject == "History":
+# #         break
+# #     print(subject)
+    
+# #Skip over sicence but print out the rest of the list
+# # for subject in subjects:
+# #     if subject == "Science":
+# #         continue
+# #     print(subject)
+
+
+# list1000 = list(range(1, 1001))
+# #break when you reach a number greater than 599
+# for number in list1000:
+#     if number > 599:
+#         break
+#     print(number)
+
+# for number in list1000:
+#     if 300 <= number <= 500:
+#         continue
+#     print(number)
+
+
+# applicants_for_credit = ["Alice", "Bob", "Charlie", "David", "Eve"]
+# credit_scores = [720, 680, 590, 610, 750]
+# for applicant, score in zip(applicants_for_credit, credit_scores):
+#     if score < 600:
+#         continue
+#     print(applicant + " approved with score: " + str(score))
+
+
 
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
-
+for index in range(len(subjects)-1):
+    print("Subject " + str(index) + ": " + subjects[index])
 
 # Given:
 numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+
+numbers = [5, 10, 15, 20]
+total = 0
+for number in numbers:
+    total += number
+print("Total: " ,total)
+
+new_numbers = list(range(1, 261))
+# Challenge: Sum up all numbers from range
+# Print total
+total = 0
+for number in new_numbers:
+    total += number
+print("Total: " ,total)
